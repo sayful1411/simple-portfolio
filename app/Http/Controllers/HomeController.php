e14e54b5c5ce0191aa710ff7e7a0d734d1299bc2
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2023-10-24 20:32:16 +0600

    update view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,20 +5,14 @@ namespace App\Http\Controllers;
 class HomeController extends Controller
 {
 
-    // home page
     public function index(){
-        // home page intro and about data
+        // load home page intro and about data
         $introData = json_decode(file_get_contents(storage_path('data/intro.json')));
         $aboutData = json_decode(file_get_contents(storage_path('data/about.json')));
-        // social link and footer contact information
-        $socialData = json_decode(file_get_contents(storage_path('data/social.json')));
-        $contactData = json_decode(file_get_contents(storage_path('data/contact.json')));
 
         return view('pages.index')->with([
             'introData' => $introData,
-            'aboutData' => $aboutData,
-            'socialData' => $socialData,
-            'contactData' => $contactData
+            'aboutData' => $aboutData
         ]);
     }
 
@@ -27,15 +21,10 @@ class HomeController extends Controller
         // education and experience data
         $educationData = json_decode(file_get_contents(storage_path('data/education.json')));
         $experienceData = json_decode(file_get_contents(storage_path('data/experience.json')));
-        // social link and footer contact information
-        $socialData = json_decode(file_get_contents(storage_path('data/social.json')));
-        $contactData = json_decode(file_get_contents(storage_path('data/contact.json')));
 
         return view('pages.resume')->with([
             'educationData' => $educationData,
-            'experienceData' => $experienceData,
-            'socialData' => $socialData,
-            'contactData' => $contactData
+            'experienceData' => $experienceData
         ]);
     }
 
@@ -43,14 +32,9 @@ class HomeController extends Controller
     public function projects(){
         // project data
         $projectsData = json_decode(file_get_contents(storage_path('data/projects.json')));
-        // social link and footer contact information
-        $socialData = json_decode(file_get_contents(storage_path('data/social.json')));
-        $contactData = json_decode(file_get_contents(storage_path('data/contact.json')));
 
         return view('pages.projects')->with([
-            'projectsData' => $projectsData,
-            'socialData' => $socialData,
-            'contactData' => $contactData
+            'projectsData' => $projectsData
         ]);
     }
 
@@ -58,17 +42,11 @@ class HomeController extends Controller
     public function showProject($id){
         // get single project data
         $project = $this->getProjectById($id);
-        // social link and footer contact information
-        $socialData = json_decode(file_get_contents(storage_path('data/social.json')));
-        $contactData = json_decode(file_get_contents(storage_path('data/contact.json')));
 
         // if project exist then show project data
         if ($project) {
             return view('pages.single-project')->with([
-                'project' => $project,
-                'socialData' => $socialData,
-                'contactData' => $contactData
-
+                'project' => $project
             ]);
         } else {
             echo "Not Found";
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // social link and footer contact information
+        $socialData = json_decode(file_get_contents(storage_path('data/social.json')));
+        $contactData = json_decode(file_get_contents(storage_path('data/contact.json')));
+
+        View::share('socialData', $socialData);
+        View::share('contactData', $contactData);
     }
 }
