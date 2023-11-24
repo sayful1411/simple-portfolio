f2d0d60bb4c5634a0e8f5e846eea020483230cb0
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2023-11-24 09:06:29 +0600

    view file sturcture changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,19 +6,16 @@ class HomeController extends Controller
 {
 
     public function index(){
-        // load home page intro and about data
         $introData = json_decode(file_get_contents(storage_path('data/intro.json')));
         $aboutData = json_decode(file_get_contents(storage_path('data/about.json')));
 
-        return view('pages.index')->with([
+        return view('index')->with([
             'introData' => $introData,
             'aboutData' => $aboutData
         ]);
     }
 
-    // resume page
     public function resume(){
-        // education and experience data
         $educationData = json_decode(file_get_contents(storage_path('data/education.json')));
         $experienceData = json_decode(file_get_contents(storage_path('data/experience.json')));
 
@@ -28,9 +25,7 @@ class HomeController extends Controller
         ]);
     }
 
-    // portjects page
     public function projects(){
-        // project data
         $projectsData = json_decode(file_get_contents(storage_path('data/projects.json')));
 
         return view('pages.projects')->with([
@@ -38,9 +33,7 @@ class HomeController extends Controller
         ]);
     }
 
-    // single project page
     public function showProject($id){
-        // get single project data
         $project = $this->getProjectById($id);
 
         // if project exist then show project data
@@ -54,9 +47,8 @@ class HomeController extends Controller
         }
     }
 
-    // retrieve single project data by id
     private function getProjectById($id) {
-        // try diffrent way to get data into array format
+        // try different way to get data into array format
         $projectsData = json_decode(file_get_contents(storage_path('data/projects.json')), true);
 
         foreach ($projectsData as $project) {
